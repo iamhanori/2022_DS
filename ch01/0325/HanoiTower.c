5fe1d145e230e388bd0bbc0503daff60cdd12f0c
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-03-25 12:11:44 +0900

    hanoitower

---
+++ ch01/0325/HanoiTower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void HanoiTower(int n, char a, char b, char c) {
+	if (n == 1) printf("원판 %d, %c -> %c \n", n, a, c); // 1번 이동
+	else{
+		HanoiTower(n - 1, a, c, b); // (n-1)개를 출발지 -> 경유지
+		printf("원판 %d, %c -> %c\n", n, a, c); // ㅜ번 원판 이동
+		HanoiTower(n - 1, b, a, c); // (n-1)개를 경유지 -> 도착지
+
+	}
+}	
+
+int main(void) {
+	int n = 4;
+	HanoiTower(n, 'A', 'B', 'C'); // A는 출발지, B는 경유지, C는 목적지
+
+	return 0;
+}
